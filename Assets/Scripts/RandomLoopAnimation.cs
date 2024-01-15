3f734e3c5fc542537adb73ff02895a4253437ffe
deenpuru <deenpuru@gmail.com>
2024-01-16 00:12:55 +0200

    trashcananimation

--- Assets/Scripts/RandomLoopAnimation.cs
+++ Assets/Scripts/RandomLoopAnimation.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class RandomLoopAnimation : MonoBehaviour
 {
-    public bool _notInteracted;
 
     [SerializeField] private int _minRange;
     [SerializeField] private int _maxRange;
-    [SerializeField] private string _animationTrigger;
+    [SerializeField] private string _defaultAnimationTrigger;
+    [SerializeField] private string _interactedAnimationTrigger;
+
 
     [Space(20)]
     [Header("-------------------Character's Animation-------------------")]
     [Space(20)]
 
     [SerializeField] private Animator _trashCanWhileInteracting;
+
+    [SerializeField] private Ping_Pong_Dialogue_Controller _dialogueController;
+
+    private bool _notInteracted = true;
 
     //Trash can NPC loop animation
 
@@ -23,22 +28,31 @@ public class RandomLoopAnimation : MonoBehaviour
         StartCoroutine(LoopAnimation());
     }
 
+    private void Update()
+    {
+        if (_dialogueController._checkDialogueON && _notInteracted)
+        {
+            _notInteracted = false;
+            _trashCanWhileInteracting.SetTrigger(_defaultAnimationTrigger);
+        }
+    }
+    private void PauseAnimation()
+    {
+       if (_dialogueController._checkDialogueON)
+        {
+            _trashCanWhileInteracting.SetTrigger(_interactedAnimationTrigger);
+        }
+      
+    }
+
+
     private IEnumerator LoopAnimation()
     {
-        while (true)
+        while (_notInteracted)
         {
-            if (_notInteracted)
-            {
                 int testRandomTime = Random.Range(_minRange, _maxRange);
-                GetComponent<Animator>().SetTrigger(_animationTrigger);
+                GetComponent<Animator>().SetTrigger(_defaultAnimationTrigger);
                 yield return new WaitForSeconds(testRandomTime);
-            }
-            else if (!_notInteracted)
-            {
-                _trashCanWhileInteracting.SetTrigger("isBlinking");
-                //yield return null;
-            }
-           
         }
     }
 }
