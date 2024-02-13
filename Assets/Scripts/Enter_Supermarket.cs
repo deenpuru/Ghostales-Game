8d2ea854314bb8d5e90fdd29ba3f678441f2e431
deenpuru <deenpuru@gmail.com>
2024-02-13 23:33:12 +0200

    Transition animation

--- Assets/Scripts/Enter_Supermarket.cs
+++ Assets/Scripts/Enter_Supermarket.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Enter_Supermarket : MonoBehaviour
 {
     [SerializeField] private PunkKid_MainPlayerControls _kid;
 
     [SerializeField] private Animator _supermarketAnimation;
+
+    [SerializeField] private PlayableDirector _playableDirector;
+    [SerializeField] private PlayableAsset _show;
+    [SerializeField] private PlayableAsset _hide;
+
+    [SerializeField] private GameObject _transition_Container;
+
+    [SerializeField] private float _animDelay = 3;
     public Transform _pointToGo;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -30,9 +39,22 @@ public class Enter_Supermarket : MonoBehaviour
         _kid.movement.y = 1;
         _kid.animator.SetFloat("Vertical", _kid.movement.y);
         _kid.animator.SetFloat("Speed", 1);
-        yield return new WaitForSeconds(1f);
+
+        
+        _playableDirector.playableAsset = _hide;
+        _playableDirector.Play();
+
+        yield return new WaitForSeconds((float)_hide.duration + _animDelay );
+
+        
+
         _kid.transform.position = _pointToGo.position;
         _kid.IsAbleToMove = true;
+
+        _playableDirector.playableAsset = _show;
+        _playableDirector.Play();
+
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
