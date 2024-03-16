f7f232c9fb86fa82ae03b73ae18357103dbae571
deenpuru <deenpuru@gmail.com>
2024-03-16 20:09:12 +0200

    CameraBounds

--- Assets/Scripts/TeleportSystem.cs
+++ Assets/Scripts/TeleportSystem.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,12 +17,20 @@ public class TeleportSystem : MonoBehaviour
     [SerializeField] private GameObject _transition_Container;
 
     [SerializeField] private float _animDelay = 3;
+    [SerializeField] private float _movementDelay;
 
-    [SerializeField] private bool _walkUp;
+    [SerializeField] private bool _PlayerWalkingUp;
+    [SerializeField] private bool _PlayerWalkingDown;
+    [SerializeField] private bool _PlayerWalkingRight;
+    [SerializeField] private bool _PlayerWalkingLeft;
 
     [SerializeField] private float _gizmosSize = 1;
     [SerializeField] private Vector3 _offsetPosition;
     [SerializeField] private Color _gizmosColor;
+
+    [SerializeField] private CinemachineConfiner2D _confiner2D;
+    [SerializeField] private PolygonCollider2D _MapBounding;
+
 
     public Transform _pointToGo;
 
@@ -43,19 +52,28 @@ public class TeleportSystem : MonoBehaviour
         _kid.movement.x = 0;
         _kid.movement.y = 0;
         _kid.animator.SetFloat("Speed", 0);
-        yield return new WaitForSeconds(1.5f);
-        _kid.movement.x = 0;
+        yield return new WaitForSeconds(_movementDelay);
 
-        if (_walkUp)
+        if (_PlayerWalkingUp)
         {
             _kid.movement.y = 1;
         }
-        else
+        else if (_PlayerWalkingDown)
         {
             _kid.movement.y = -1;
         }
-        
+        else if (_PlayerWalkingRight)
+        {
+            _kid.movement.x = 1;
+        }
+        else if (_PlayerWalkingLeft)
+        {
+            _kid.movement.x = -1;
+        }
+
+
         _kid.animator.SetFloat("Vertical", _kid.movement.y);
+        _kid.animator.SetFloat("Horizontal", _kid.movement.x);
         _kid.animator.SetFloat("Speed", 1);
 
         _playableDirector.playableAsset = _hide;
@@ -63,8 +81,15 @@ public class TeleportSystem : MonoBehaviour
 
         yield return new WaitForSeconds((float)_hide.duration + _animDelay );
 
+        //TO DO: turn off the cinemachine confiner 2D component
+
+        _confiner2D.m_BoundingShape2D = _MapBounding;
+
         _kid.transform.position = new Vector3(_pointToGo.position.x + _offsetPosition.x, _pointToGo.position.y + _offsetPosition.y, _pointToGo.position.z + _offsetPosition.z);
         _kid.IsAbleToMove = true;
+
+        //TO DO: turn on the cinemachine confiner 2D component
+        yield return new WaitForSeconds(0.2f);
 
         _playableDirector.playableAsset = _show;
         _playableDirector.Play();
